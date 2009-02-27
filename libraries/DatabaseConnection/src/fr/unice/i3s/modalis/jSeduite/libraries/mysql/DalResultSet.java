1862e2b94866d06eca1f182afb3958189f09d35e
sebastien.mosser <sebastien.mosser@54f14dfa-0325-11de-99c0-75d572ba1ddd>
2009-02-27 10:26:25 +0000

    Database initialization files
    git-svn-id: http://jseduite.googlecode.com/svn/trunk@7 54f14dfa-0325-11de-99c0-75d572ba1ddd

--- libraries/DatabaseConnection/src/fr/unice/i3s/modalis/jSeduite/libraries/mysql/DalResultSet.java
+++ libraries/DatabaseConnection/src/fr/unice/i3s/modalis/jSeduite/libraries/mysql/DalResultSet.java
@@ -1,25 +1,52 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+/**
+ * This file is part of jSeduite::DatabaseConnection
+ *
+ * Copyright (C) 2008-  Sebastien Mosser
+ *
+ * jSeduite::DatabaseConnection is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * jSeduite::DatabaseConnection is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with jSeduite::DatabaseConnection; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * @author Sébastien Mosser [mosser@polytech.unice.fr]
+ *
+ **/
 
 package fr.unice.i3s.modalis.jSeduite.libraries.mysql;
+
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.Hashtable;
 
-/**
- *
+/** Data Structure to handle data extracted from a database throught the DAL
  * @author mosser
  */
 public class DalResultSet {
 
-
+    /** Bind a string label to a column integer position
+     */
     public Hashtable<String,Integer> labels;
+
+    /** Number of columns inside this result set
+     */
+    public int nbColumns;
+
     private int index;
     private String[][] content;
-    public int nbColomns;
 
+    /** Build a DalResultSet using a database ResultSet as data container
+     * @param r the ResultSet usd to fill the instance
+     * @throws DALException
+     */
     public DalResultSet(ResultSet r) throws DALException {
         try {
             this.index = 0;
@@ -30,7 +57,7 @@ public class DalResultSet {
 
             ArrayList<String[]> tmp = new ArrayList<String[]>();
             int colCount = metadata.getColumnCount();
-            nbColomns = metadata.getColumnCount();
+            nbColumns = metadata.getColumnCount();
             while(r.next()) {
                 String[] row = new String[colCount+1];
                 for(int i = 1; i < colCount+1; i++) {
@@ -44,11 +71,23 @@ public class DalResultSet {
         }
     }
 
+    /** forward to the next result
+     */
     public void next() { index++; }
+    
+    /** rewind to the previous result
+     */
     public void pred() { index--; }
 
+    /** Get the size (ie the number of tuples) of this DalResultSet
+     * @return the size of this data set
+     */
     public int size() { return content.length; }
 
+    /** Retrieve the value of an attribute on the current tuple
+     * @param name the column name
+     * @return the attribute value
+     */
     public String getValue(String name) {
         if (labels.get(name)!=null)
             return content[index][labels.get(name).intValue()];
--- libraries/DatabaseConnection/src/fr/unice/i3s/modalis/jSeduite/libraries/mysql/DataAccessLayer.java
+++ libraries/DatabaseConnection/src/fr/unice/i3s/modalis/jSeduite/libraries/mysql/DataAccessLayer.java
@@ -1,8 +1,25 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
+/**
+ * This file is part of jSeduite::DatabaseConnection
+ *
+ * Copyright (C) 2008-  Sebastien Mosser
+ *
+ * jSeduite::DatabaseConnection is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * jSeduite::DatabaseConnection is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with jSeduite::DatabaseConnection; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * @author Sébastien Mosser [mosser@polytech.unice.fr]
+ *
+ **/
 package fr.unice.i3s.modalis.jSeduite.libraries.mysql;
 
 import javax.naming.*;
@@ -10,40 +27,74 @@ import java.sql.*;
 import java.util.ArrayList;
 import javax.sql.DataSource;
 
-/**
- *
+/** A MySQL DataAccessLayer (aka DAL) implementation
  * @author mosser
  */
 public class DataAccessLayer {
 
-    private static final String DEFAULT_JNDI = "jdbc/jSeduite";
-    
+    private static final String DEFAULT_JNDI = "jdbc/jseduite";
     private String jndiName;
+    private boolean shouldLog = false;
 
+    /** Build a DAL using the default jSeduite JDBC resource
+     */
     public DataAccessLayer() { this.jndiName = DEFAULT_JNDI; }
+
+    /** Build a DAL using a user-given JDBC resource JNDI name
+     * @param url the JNDI name of the wanted JDBC resource
+     */
     public DataAccessLayer(String url) { this.jndiName = url; }
 
+    /** (De)Activate the query log mechanism
+     * @param b true to activate, false to deactivate
+     * @remarks this mechanism is deactivated by default
+     */
+    public void setQueryLog(boolean b) { this.shouldLog = b; }
+
+    /** Log a string, if explicitely asked for (basically print it to stdout)
+     * @param l the log to print
+     */
+    private void log(String l) {
+        if (this.shouldLog)
+            System.out.println(l);
+    }
+
+    /** Build a connection to the database
+     * @return A Connection to the database through the MySQL JDBC driver
+     * @throws DALException
+     */
     private Connection buildConnection() throws DALException {
+        log("Opening connection");
         try {
             InitialContext ictx = new InitialContext();
             DataSource ds = (DataSource) ictx.lookup(this.jndiName);
             return ds.getConnection();
        }
        catch(Exception e) {
-           e.printStackTrace();
            throw new DALException(e.getMessage());
        }
     }
 
+    /** Close a connection previously opened to the database
+     * @param c the connection toc lode
+     * @throws DALException
+     */
     private void closeConnection(Connection c) throws DALException {
+        log("Closing connection");
         try { c.close(); }
         catch(Exception e) { throw new DALException(e.getMessage()); }
     }
 
+
+    /** Execute a query which does not produce any output result (eg insert)
+     * @param query the SQL (MySQL dialect) query to execute
+     * @throws DALException
+     */
     public void executeVoid(String query) throws DALException {
         Connection c = buildConnection();
         try {
             Statement s = c.createStatement();
+            log("executeVoid ["+s+"]");
             s.executeUpdate(query);
             return ;
         }catch (Exception e) {
@@ -53,11 +104,18 @@ public class DataAccessLayer {
     }
 
 
+    /** Extract a scalar value from the database 
+     * @param query the query to execute
+     * @param column the column's name of the scalar value to retrieve
+     * @return a single string containing the scalar
+     * @throws DALException (no result for query ...)
+     */
     public String extractScalar(String query, String column)
             throws DALException {
         Connection c = buildConnection();
         try {
             Statement s = c.createStatement();
+            log("extractScalar ["+s+"]");
             ResultSet r = s.executeQuery(query);
             if ( ! r.first())
                 throw new DALException("No result for [" + query + "]");
@@ -68,11 +126,18 @@ public class DataAccessLayer {
         } finally { closeConnection(c);}
     }
 
+    /** Extract a set of scalar values from the database
+     * @param query the query to execute
+     * @param column the column's name of the scalar value to retrieve
+     * @return an array of String containing all the expected values
+     * @throws DALException
+     */
     public String[] extractScalarSet(String query, String column)
             throws DALException {
         Connection c = buildConnection();
         try {
             Statement s = c.createStatement();
+            log("extractScalarSet ["+s+"]");
             ResultSet r = s.executeQuery(query);
             ArrayList<String> result = new ArrayList<String>();
             while (r.next())
@@ -84,6 +149,11 @@ public class DataAccessLayer {
         }  finally { closeConnection(c);}
     }
 
+    /** Extract a complex dataset from the database
+     * @param query the query to execute
+     * @return a DalResultSet containing all the result extracted from the DB
+     * @throws fr.unice.i3s.modalis.jSeduite.libraries.mysql.DALException
+     */
     public DalResultSet extractDataSet(String query) throws DALException {
         Connection c = buildConnection();
         try {
