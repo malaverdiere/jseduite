9c3da3e2294d962a55e92da772bc2f5cc8074e64
sebastien.mosser <sebastien.mosser@54f14dfa-0325-11de-99c0-75d572ba1ddd>
2009-03-03 13:21:02 +0000

    licence LGPL
    git-svn-id: http://jseduite.googlecode.com/svn/trunk@24 54f14dfa-0325-11de-99c0-75d572ba1ddd

--- webservices/TvHelper/src/java/fr/unice/i3s/modalis/jSeduite/technical/tv/TvHelper.java
+++ webservices/TvHelper/src/java/fr/unice/i3s/modalis/jSeduite/technical/tv/TvHelper.java
@@ -1,7 +1,25 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+/**
+ * This file is part of jSeduite::TvHelper
+ *
+ * Copyright (C) 2008-  Sebastien Mosser
+ *
+ * jSeduite::TvHelper is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * jSeduite::TvHelper is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with jSeduite::UserProfile; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * @author      Main Sébastien Mosser          [mosser@polytech.unice.fr]
+ *
+ **/
 
 package fr.unice.i3s.modalis.jSeduite.technical.tv;
 
