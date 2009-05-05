7fb9a934bc0d6275cb82bb9d15299e094f4c4c70
yichenzhao18@gmail.com <yichenzhao18@gmail.com@54f14dfa-0325-11de-99c0-75d572ba1ddd>
2009-05-05 02:33:51 +0000

    i think we don't need the link table between day and break so I delete it.
    git-svn-id: http://jseduite.googlecode.com/svn/trunk@128 54f14dfa-0325-11de-99c0-75d572ba1ddd

--- libraries/DatabaseSchemas/src/Schemas/BreakManager.sql
+++ libraries/DatabaseSchemas/src/Schemas/BreakManager.sql
@@ -29,24 +29,17 @@ CREATE TABLE `break_time` (
 `start` VARCHAR(20) NOT NULL ,
 `end` VARCHAR(20) NOT NULL ,
 `kind` VARCHAR(10) NOT NULL ,
+`day` VARCHAR(10) NOT NULL,
 PRIMARY KEY (`break_id`)
 );
 
-DROP TABLE IF EXISTS `break_time_days_lnk`;
-CREATE TABLE `break_time_days_lnk` (
- `break_id` INT(11) NOT NULL REFERENCES `break_time`,
- `day` VARCHAR(10) NOT NULL,
-  PRIMARY KEY(`break_id`,`day`)
-  );
 
 DROP VIEW IF EXISTS `break_time_today`;
-CREATE VIEW `break_time_today` AS
-    SELECT `promos`.`code` AS `p_code`, `promos`.`name` AS `promo`,
-           `break_time`.`start` AS  `start_time`,
-           `break_time`.`end` AS `end_time`,
-           `break_time`.`kind` AS `break_type`, `day`
-    FROM `break_time`, `promos`,`break_time_days_lnk`
-    WHERE `break_time_days_lnk`.`day`= dayname(curdate()) AND
-          `break_time_days_lnk`.`break_id`=`break_time`.`break_id` AND
-          `promos`.`id`=`break_time`.`promo`;
+ CREATE VIEW `break_time_today` AS
+ SELECT
+`break_time`.`break_id` AS `break_id`,`promos`.`name` AS `promo`,`break_time`.`start` AS  `start_time`, `break_time`.`end` AS `end_time`,`break_time`.`kind` AS `break_type`,`break_time`.`day`
+FROM
+ `break_time`, `promos`
+ WHERE
+ `break_time`.`day`= dayname(curdate()) AND `promos`.`promos_id`=`break_time`.`promo`;
 
