6eab293591feff554e1044068b2daf184856e647
bienvenueqin <bienvenueqin@54f14dfa-0325-11de-99c0-75d572ba1ddd>
2009-05-09 14:22:16 +0000

    there is the initial data in this sql
    git-svn-id: http://jseduite.googlecode.com/svn/trunk@140 54f14dfa-0325-11de-99c0-75d572ba1ddd

---
+++ libraries/DatabaseSchemas/src/Data/Alarms_int.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO `alarms` VALUES (NULL,1,"you have to change the classrome 222 in the next class ");
+INSERT INTO `alarms` VALUES (NULL,2,"Because your teacher is ill, you will be teached by professor qin"); 
+INSERT INTO `alarms` VALUES (NULL,3,"Because the H1N1 flu, you are free"); 
