9588a3faaf8f0045203f81ea93e66cb7c43c8893
yichenzhao18@gmail.com <yichenzhao18@gmail.com@54f14dfa-0325-11de-99c0-75d572ba1ddd>
2009-05-18 05:50:09 +0000

    git-svn-id: http://jseduite.googlecode.com/svn/trunk@157 54f14dfa-0325-11de-99c0-75d572ba1ddd

--- libraries/DatabaseSchemas/src/Schemas/Alarms.sql
+++ libraries/DatabaseSchemas/src/Schemas/Alarms.sql
@@ -1,14 +1,15 @@
+﻿
 DROP TABLE IF EXISTS `alarms`;
 CREATE TABLE `alarms` (
-`alarm_id` INT(11) NOT NULL AUTO_INCREMENT ,
-`binding`  INT(11) NOT NULL REFERENCES `break_time`,
-`content` VARCHAR(255) NOT NULL ,
+`alarm_id` INT(11) NOT NULL AUTO_INCREMENT,
+`content`  VARCHAR(255) NOT NULL REFERENCES `alarms`,
 PRIMARY KEY (`alarm_id`)
 );
 
 
-INSERT INTO `alarms` VALUES (NULL,1,"you have to change the classrome 222 in the next class ");
-INSERT INTO `alarms` VALUES (NULL,2,"Because your teacher is ill, you will be teached by professor qin"); 
-INSERT INTO `alarms` VALUES (NULL,3,"Because the H1N1 flu, you are free"); 
-INSERT INTO `alarms` VALUES (NULL,4,"free");
-
+DROP TABLE IF EXISTS `break_alarm_lnk`;
+CREATE TABLE `break_alarm_lnk` (
+`break` INT(11) NOT NULL REFERENCES `break_time`,
+`binding_alarm`  INT(11) NOT NULL REFERENCES `alarms`,
+PRIMARY KEY (`break`,`binding_alarm`)
+);
--- libraries/DatabaseSchemas/src/Schemas/BreakManager.sql
+++ libraries/DatabaseSchemas/src/Schemas/BreakManager.sql
@@ -1,31 +1,21 @@
-/** This file is part of jSeduite::Database Schemas
- *
- * Copyright (C) 2008-  Sebastien Mosser
- *
- * jSeduite::DatabaseSchema is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * jSeduite::DatabaseSchema is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with jSeduite::DatabaseSchema; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
- *
- * @author      Main Zhao Yichen      [yichenzhao18@gmail.com]
- * @author      Main Qin Zhaobo       [Bienvenueqin@gmail.com]
- * @contributor 2009 Mosser Sebastien [mosser@polytech.unice.fr]
- * @depends     "SchoolLife.sql"
-**/
+﻿DROP TABLE IF EXISTS `promos`;
+CREATE TABLE `promos` (
+`promos_id` INT(11) NOT NULL AUTO_INCREMENT ,
+`code` VARCHAR(50) NOT NULL ,
+`name` VARCHAR(255) NOT NULL ,
+PRIMARY KEY (`promos_id`)
+);
+
+DROP TABLE IF EXISTS `break_time_promos_lnk`;
+CREATE TABLE `break_time_promos_lnk` (
+`break` INT(11) NOT NULL REFERENCES `break_time`,
+`binding_alarm` INT(11) NOT NULL REFERENCES `promos`,
+PRIMARY KEY (`break`,`binding_alarm`)
+);
 
 DROP TABLE IF EXISTS `break_time`;
 CREATE TABLE `break_time` (
 `break_id` INT(11) NOT NULL AUTO_INCREMENT ,
-`promo`    INT(11) NOT NULL REFERENCES `promos`,
 `start` VARCHAR(20) NOT NULL ,
 `end` VARCHAR(20) NOT NULL ,
 `kind` VARCHAR(10) NOT NULL ,
@@ -42,4 +32,6 @@ FROM
  `break_time`, `promos`
  WHERE
  `break_time`.`day`= dayname(curdate()) AND `promos`.`promos_id`=`break_time`.`promo`;
+
+
 
