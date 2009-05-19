2efedc897ca9ad69bdc58654d31f6d1c0774d77c
yichenzhao18@gmail.com <yichenzhao18@gmail.com@54f14dfa-0325-11de-99c0-75d572ba1ddd>
2009-05-19 06:30:08 +0000

    git-svn-id: http://jseduite.googlecode.com/svn/trunk@160 54f14dfa-0325-11de-99c0-75d572ba1ddd

---
+++ libraries/DatabaseSchemas/src/Schemas/Alarms.sql
@@ -0,0 +1,15 @@
+﻿
+DROP TABLE IF EXISTS `alarms`;
+CREATE TABLE `alarms` (
+`alarm_id` INT(11) NOT NULL AUTO_INCREMENT,
+`content`  VARCHAR(255) NOT NULL,
+PRIMARY KEY (`alarm_id`)
+);
+
+
+DROP TABLE IF EXISTS `break_alarm_lnk`;
+CREATE TABLE `break_alarm_lnk` (
+`break` INT(11) NOT NULL REFERENCES `break_time`,
+`binding_alarm`  INT(11) NOT NULL REFERENCES `alarms`,
+PRIMARY KEY (`break`,`binding_alarm`)
+);
