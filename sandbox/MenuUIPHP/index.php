60485a19befca97ab79f078c92b2a90b48b9586a
Steve.Colombie <Steve.Colombie@54f14dfa-0325-11de-99c0-75d572ba1ddd>
2009-05-29 08:56:55 +0000

    All the Finder and CRUD services can be used. Implementation is encapsuled in a Proxy class. The UI has to be done.
    git-svn-id: http://jseduite.googlecode.com/svn/trunk@196 54f14dfa-0325-11de-99c0-75d572ba1ddd

--- sandbox/MenuUIPHP/index.php
+++ sandbox/MenuUIPHP/index.php
@@ -1,5 +1,5 @@
 <?php
-	require_once('util.php');
+    require_once('jseduite_proxies.php');
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
@@ -9,10 +9,34 @@
     </head>
     <body>
         <?php
-            displayAllCourses();
-            deleteCourse('desert_name_166929727');
-            echo "----------------------------<br/>";
-            displayAllCourses();
+            $finderProxy = new CourseFinderProxy ();
+            $crudProxy = new CourseCRUDProxy();
+
+            $course = new Course(array('kind' => "type", 'name' => "znom"));
+            $crudProxy->create($course->getStructure());
+            $course = new Course(array('kind' => "type", 'name' => "zznom"));
+            $crudProxy->create($course->getStructure());
+
+            echo "<pre>";
+            print_r($finderProxy->getAllCoursesReferences());
+            echo "</pre>-------------<br/>";
+
+            $courseRes = $finderProxy->findCourseByName("znom");
+            $course = new Course($courseRes);
+            $crudProxy->delete($course->getStructure());
+
+            $courseRes = $crudProxy->read(refMaker("zznom"));
+            $course = new Course($courseRes);
+            $course->setKind("nouveau_type");
+            $crudProxy->update($course->getStructure());
+
+            $courseRes = $finderProxy->findCourseByName("zznom");
+            print_r($courseRes);
+
+            echo "<pre>";
+            print_r($finderProxy->getAllCoursesReferences());
+            echo "</pre>";
+
         ?>
     </body>
 </html>
