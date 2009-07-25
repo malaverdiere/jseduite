71e80dd7502669b204ff9d56700d7e9d984aed46
sebastien.mosser <sebastien.mosser@54f14dfa-0325-11de-99c0-75d572ba1ddd>
2009-07-25 13:22:05 +0000

    cleaning up the attic
    git-svn-id: http://jseduite.googlecode.com/svn/trunk@247 54f14dfa-0325-11de-99c0-75d572ba1ddd

--- attic/PictureAlbums/src/Description/transform.sh
+++
@@ -1,6 +0,0 @@
-source ~/.bash_profile
-
-dot -Tpng -o arch.png -Nfontname=Courier \
-    -Gfontpath=/System/Library/Fonts arch.dot
-dot -Tpng -o business.png -Nfontname=Courier \
-    -Gfontpath=/System/Library/Fonts business.dot
