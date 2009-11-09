eac10e2ce99fd631255a3bda2ec5d094e02b2128
sebastien.mosser <sebastien.mosser@54f14dfa-0325-11de-99c0-75d572ba1ddd>
2009-11-09 12:17:33 +0000

    adding architectural contracts
    git-svn-id: http://jseduite.googlecode.com/svn/trunk@343 54f14dfa-0325-11de-99c0-75d572ba1ddd

--- architecture/add_bpel_contract.sh
+++ architecture/add_bpel_contract.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
 ORCH_NAME=$1
+DIR_NAME=$2
 DIR=contracts
 
-WSDL=../orchestrations/$ORCH_NAME/src/$ORCH_NAME.wsdl
-XSD=../orchestrations/$ORCH_NAME/src/$ORCH_NAME.xsd
+if [ "$DIR_NAME" = "" ] 
+then
+    XSD=../orchestrations/$ORCH_NAME/src/$ORCH_NAME.xsd
+    WSDL=../orchestrations/$ORCH_NAME/src/$ORCH_NAME.wsdl
+else
+    XSD=../orchestrations/$DIR_NAME/src/$ORCH_NAME.xsd
+    WSDL=../orchestrations/$DIR_NAME/src/$ORCH_NAME.wsdl
+fi
 
 echo "# Retrieving XSD data model [Partners => '']"
 echo "$XSD -> $DIR/$ORCH_NAME.xsd"
