1ef01821c8bcc20b2d279f29eb88b061b4a88428
descl.zouig <descl.zouig@54f14dfa-0325-11de-99c0-75d572ba1ddd>
2010-06-28 08:55:44 +0000

    new WebService Clock
    git-svn-id: http://jseduite.googlecode.com/svn/trunk@431 54f14dfa-0325-11de-99c0-75d572ba1ddd

---
+++ webservices/Clock/src/java/fr/unice/i3s/modalis/jSeduite/technical/time/Clock/Clock.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package fr.unice.i3s.modalis.jSeduite.technical.time.Clock;
+
+import javax.jws.WebService;
+
+/**
+ *
+ * @author descl
+ */
+@WebService()
+public class Clock {
+
+}
