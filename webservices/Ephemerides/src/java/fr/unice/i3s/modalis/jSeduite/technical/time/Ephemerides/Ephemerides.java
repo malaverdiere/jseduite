87c6ce18be02fed1415bd2f2767c17b859758086
descl.zouig <descl.zouig@54f14dfa-0325-11de-99c0-75d572ba1ddd>
2010-07-15 08:00:07 +0000

    adding Ephemerides WebWService
    git-svn-id: http://jseduite.googlecode.com/svn/trunk@441 54f14dfa-0325-11de-99c0-75d572ba1ddd

---
+++ webservices/Ephemerides/src/java/fr/unice/i3s/modalis/jSeduite/technical/time/Ephemerides/Ephemerides.java
@@ -0,0 +1,44 @@
+/**
+ * This file is part of jSeduite::util
+ *
+ * Copyright (C) 2008-  Sebastien Mosser
+ *
+ * jSeduite::util is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * jSeduite::util is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with jSeduite::util; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * @contributor 2010     Main Christophe Desclaux    [desclaux@polytech.unice.fr]
+ **/
+
+package fr.unice.i3s.modalis.jSeduite.technical.time.Ephemerides;
+import fr.unice.i3s.modalis.jSeduite.libraries.mysql.*;
+
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
+
+@WebService()
+public class Ephemerides {
+
+    @WebMethod(operationName = "getEphemerides")
+    public String[] getEphemerides(int day, int month) throws EphemeridesException{
+        DataAccessLayer dal = new DataAccessLayer();
+        String sql = "SELECT * FROM `ephemerides WHERE day = `"+day+"` AND month = `"+month+"`;";
+        try {
+            return dal.extractScalarSet(sql, "name");
+        } catch(Exception e) {
+            throw new EphemeridesException(e.getMessage());
+        }
+    }
+}
