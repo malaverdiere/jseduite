e3f60c4cc568f41b192b30417ca767a4e43a2285
descl.zouig <descl.zouig@54f14dfa-0325-11de-99c0-75d572ba1ddd>
2010-07-21 09:01:32 +0000

    add licence in MenuTools.java
    git-svn-id: http://jseduite.googlecode.com/svn/trunk@451 54f14dfa-0325-11de-99c0-75d572ba1ddd

--- webservices/MenuService/src/java/fr/unice/i3s/modalis/jSeduite/technical/restaurant/MenuTools.java
+++ webservices/MenuService/src/java/fr/unice/i3s/modalis/jSeduite/technical/restaurant/MenuTools.java
@@ -1,7 +1,23 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+/** This file is part of jSeduite::MenuService
+ *
+ * Copyright (C) 2008-  Sebastien Mosser
+ *
+ * jSeduite::MenuService is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * jSeduite::MenuService is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with jSeduite::MenuService; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * @author 2010 Desclaux Christophe[desclaux@polytech.unice.fr]
+**/
 
 package fr.unice.i3s.modalis.jSeduite.technical.restaurant;
 
@@ -10,10 +26,7 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
 
-/**
- *
- * @author descl
- */
+
 public class MenuTools {
     /** A static method to transform a SQL entry into a java Date
      * @param dateString the SQL date entry to transform
